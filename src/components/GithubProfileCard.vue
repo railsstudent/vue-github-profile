3f92d044036705134a048ff9d73ee121fb8aa562
Connie Leung <cleung334@gmail.com>
2025-08-03 17:13:48 +0800

    chore: format files

--- src/components/GithubProfileCard.vue
+++ src/components/GithubProfileCard.vue
@@ -1,32 +1,27 @@
 <script setup lang="ts">
-import { useGithubProfile } from '@/composables/useGithubProfile.ts';
+import { useGithubProfile } from '@/composables/useGithubProfile.ts'
 
 type Prop = {
-    username: string;
+  username: string
 }
 
-const { username = 'railsstudent' } = defineProps<Prop>();
+const { username = 'railsstudent' } = defineProps<Prop>()
 
 const { username: name, profile, error } = useGithubProfile()
 
 name.value = username
-
 </script>
 
 <template>
-    <div v-if="profile">
-        <img :src="profile.avatar_url" :alt="profile.name" class="avatar" />
-        <p>Username: {{ profile.login }}</p>
-        <p>Name: {{ profile.name }}</p>
-        <p>Bio: {{ profile.bio || 'N/A' }}</p>
-        <p>Followers: {{ profile.followers }}</p>
-        <p>Following: {{ profile.following }}</p>
-        <p>Public Repos: {{ profile.public_repos }}</p>
-        <a :href="profile.html_url" target="_blank" rel="noopener noreferrer">
-            View Profile
-        </a>
-    </div>
-    <div v-else-if="error">
-        Error: {{ error }}
-    </div>
+  <div v-if="profile">
+    <img :src="profile.avatar_url" :alt="profile.name" class="avatar" />
+    <p>Username: {{ profile.login }}</p>
+    <p>Name: {{ profile.name }}</p>
+    <p>Bio: {{ profile.bio || 'N/A' }}</p>
+    <p>Followers: {{ profile.followers }}</p>
+    <p>Following: {{ profile.following }}</p>
+    <p>Public Repos: {{ profile.public_repos }}</p>
+    <a :href="profile.html_url" target="_blank" rel="noopener noreferrer"> View Profile </a>
+  </div>
+  <div v-else-if="error">Error: {{ error }}</div>
 </template>
--- src/components/GithubProfileList.vue
+++ src/components/GithubProfileList.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts">
 import GithubProfileCard from './GithubProfileCard.vue'
 
-const usernames = ['johnsoncodehk', 'antfu', 'railsstudent', 'danielkellyio', 'hootlex', 'MooseSaeed']
+const usernames = [
+  'johnsoncodehk',
+  'antfu',
+  'railsstudent',
+  'danielkellyio',
+  'hootlex',
+  'MooseSaeed',
+]
 </script>
 
 <template>
-    <div class="header">
-        <h1>Github Profile List</h1>
-    </div>
-    <GithubProfileCard v-for="username in usernames" :key="username"  :username="username"/>
+  <div class="header">
+    <h1>Github Profile List</h1>
+  </div>
+  <GithubProfileCard v-for="username in usernames" :key="username" :username="username" />
 </template>
 
 <style>
-    div.header {
-        grid-column: 1/3;
-        padding: 0.75rem;
-    }
+div.header {
+  grid-column: 1/3;
+  padding: 0.75rem;
+}
 
-    div > h1 {
-        text-align: center;
-    }
+div > h1 {
+  text-align: center;
+}
 </style>
