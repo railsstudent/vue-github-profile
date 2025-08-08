1943272c20ae32671429b90d7917c62e6068932c
Connie Leung <cleung334@gmail.com>
2025-08-09 07:48:52 +0800

    chore: delete the header class in the template

--- src/components/GithubProfileList.vue
+++ src/components/GithubProfileList.vue
@@ -5,7 +5,7 @@ const { usernames } = defineProps<{ usernames: string[] }>()
 </script>
 
 <template>
-  <div class="header p-[0.75rem] col-span-full">
+  <div class="p-[0.75rem] col-span-full">
     <h1 class="text-3xl text-center">Github Profile List (Vue 3 Ver.)</h1>
   </div>
   <GithubProfileCard v-for="username in usernames" :key="username" :username="username" />
