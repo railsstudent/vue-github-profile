8a733494e02996ae6e570acfdcb720d4e1c4f9e7
Connie Leung <cleung334@gmail.com>
2025-08-09 23:18:32 +0800

    refactor: deploy to github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const base = process.env.NODE_ENV === 'development' ?  '/' : '/vue-github-profile/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     tailwindcss(),
     vue(),
